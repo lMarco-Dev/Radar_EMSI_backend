991d95689b68f677b7291cb769686112735ed286
enzos112 <enzobazang@gmail.com>
2026-03-27 18:22:57 -0500

    Solución a CORS

--- src/main/java/com/emsi/security/RateLimitingFilter.java
+++ src/main/java/com/emsi/security/RateLimitingFilter.java
@@ -2,7 +2,6 @@ package com.emsi.security;
 
 import io.github.bucket4j.Bandwidth;
 import io.github.bucket4j.Bucket;
-import io.github.bucket4j.Refill;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,7 +32,12 @@ public class RateLimitingFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        if (request.getRequestURI().startsWith("/api/auth/login")) {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        if (request.getRequestURI().startsWith("/api/auth/login") && "POST".equalsIgnoreCase(request.getMethod())) {
             String ip = request.getRemoteAddr();
             Bucket bucket = cache.computeIfAbsent(ip, k -> createNewBucket());
 
@@ -42,6 +46,9 @@ public class RateLimitingFilter extends OncePerRequestFilter {
             } else {
                 response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
                 response.setContentType("application/json");
+                response.setHeader("Access-Control-Allow-Origin", "https://emsiradar.vercel.app");
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+
                 response.getWriter().write("{\"success\": false, \"message\": \"Demasiados intentos de inicio de sesión. Por favor, intenta en 1 minuto.\"}");
                 return;
             }
