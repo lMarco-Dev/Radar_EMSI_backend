78538961e4baeaaca4c1139428faf565d550b656
enzos112 <enzobazang@gmail.com>
2026-03-28 21:49:58 -0500

    Correción de zona horaria

--- src/main/java/com/emsi/EmsiApplication.java
+++ src/main/java/com/emsi/EmsiApplication.java
@@ -1,10 +1,19 @@
 package com.emsi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EmsiApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(EmsiApplication.class, args);
     }
